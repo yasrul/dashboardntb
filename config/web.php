6cfc0f26111d1f6f89daf8d9ae47507cae0204ae
yasrul <yasrul@gmail.com>
2017-10-13 17:10:48 +0800

    20171013

--- config/web.php
+++ config/web.php
@@ -7,17 +7,19 @@ $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
+    'homeUrl' => '/dashboardntb',
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'T3V4K9whYQTs7ldGlDoxcHtHNoAVUkzR',
+            'baseUrl' => '/dashboardntb',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
         'user' => [
             'identityClass' => 'app\models\User',
-            'enableAutoLogin' => true,
+            'enableAutoLogin' => false,
         ],
         'errorHandler' => [
             'errorAction' => 'site/error',
@@ -39,14 +41,18 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
+        
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                '<controller:\w+>/<id:\d+>' => '<controller>/view',
+                '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+                '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+                '<controller:\w+\-\w+>/<id:\d+>' => '<controller>/view',
             ],
         ],
-        */
+        
     ],
     'params' => $params,
 ];
